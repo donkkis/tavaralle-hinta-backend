bcb2e463eb5042d17fcc22a95025b0e22b8642e7
Panu Aho <panu.aho@gmail.com>
2020-11-12 23:06:23 +0200

    add test config

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,25 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI
+
+switch (process.env.NODE_ENV) {
+    case 'development':
+        MONGODB_URI = process.env.MONGODB_URI
+        break
+    case 'production':
+        MONGODB_URI = process.env.MONGODB_URI
+        break
+    case 'test':
+        MONGODB_URI = process.env.MONGODB_TEST_URI
+        break
+    default:
+        break
+}
+
+if (process.env.NODE_ENV === 'test') {
+    MONGODB_URI = process.env.TEST
+}
 
 module.exports = {
     MONGODB_URI,
