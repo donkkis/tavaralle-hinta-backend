147bfbcbdf66a072bfaf9772af2ae51842464bdd
Panu Aho <panu.aho@wapice.com>
2021-03-03 23:40:11 +0200

    release2 builds

---
+++ build/precache-manifest.2309e8b626b76fb2485dd9b24c43b154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b1da71778a5de08fb320a9df14570b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d064248d527106ddd3",
+    "url": "/static/css/main.87890475.chunk.css"
+  },
+  {
+    "revision": "5f29381d62e02edec26d",
+    "url": "/static/js/2.1082b4ed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1082b4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d064248d527106ddd3",
+    "url": "/static/js/main.0f728006.chunk.js"
+  },
+  {
+    "revision": "ae46ac65314d8b21f678",
+    "url": "/static/js/runtime-main.6110b058.js"
+  }
+]);
